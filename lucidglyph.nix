c2cd547e5db8f5c37cd5eeb7fbfdae45cbe3f78a
retr0astic <retr0astic@gmail.com>
2026-06-08 13:15:24 +0400

    working lucidglyph

--- lucidglyph.nix
+++ lucidglyph.nix
@@ -9,13 +9,12 @@
     rev = "v0.14.0";
     hash = "sha256-RAHGVEUS5Hkig8xtPQCcaLVsXY0ER6N4eYNnYiV56eM=";
   };
-
   lucidglyphConf = pkgs.runCommand "lucidglyph-fontconfig" {} ''
     mkdir -p $out/etc/fonts/conf.d
     ln -s ${lucidglyph}/src/modules/fontconfig/11-lucidglyph-grayscale.conf \
       $out/etc/fonts/conf.d/11-lucidglyph-grayscale.conf
-    ln -s ${lucidglyph}/src/modules/fontconfig/12-lucidglyph-slight-hinting.conf \
-      $out/etc/fonts/conf.d/12-lucidglyph-slight-hinting.conf
+    ln -s ${lucidglyph}/src/modules/fontconfig/11-lucidglyph-hinting-slight.conf \
+      $out/etc/fonts/conf.d/11-lucidglyph-hinting-slight.conf
   '';
 in {
   fonts.fontconfig = {
