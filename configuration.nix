0e53c5190a8199638586138d8bf79e1ee0dbfddf
retr0astic <sreejiraj2399@gmail.com>
2026-06-20 14:53:06 +0400

    removed firefox

--- configuration.nix
+++ configuration.nix
@@ -134,7 +134,6 @@
       # (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
     ];
   };
-  programs.firefox.enable = true;
   programs.hyprland = {
     enable = true;
     withUWSM = true;
