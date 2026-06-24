9ac53fd587811abbda220f4a6d594e33981163c4
retr0astic <sreejiraj2399@gmail.com>
2026-06-24 12:15:33 +0400

    smooth kitty

--- home.nix
+++ home.nix
@@ -62,6 +62,9 @@
       dynamic_background_opacity = true;
       background_opacity = "0.75";
       background_blur = 7;
+      cursor_trail = 200;
+      cursor_trail_decay = "0.05 0.2";
+      cursor_trail_start_threshold = 2;
     };
     font = {
       name = "JetBrainsMono Nerd Font";
