9f9ab332b4d9678d0fb22798f9a4435b30237e3b
retr0astic <sreejiraj2399@gmail.com>
2026-07-14 13:13:16 +0400

    caelesita binds

--- modules/integrations/hyprland-caelestia.nix
+++ modules/integrations/hyprland-caelestia.nix
@@ -2,7 +2,17 @@
   config.retr0astic.integrations.hyprland-caelestia = {
     desktop = "hyprland";
     shell = "caelestia";
-    home = {lib, ...}: {
+    home = {lib, ...}: let
+      inherit (lib.generators) mkLuaInline;
+
+      globalBind = key: shortcut: options: {
+        _args = [
+          (mkLuaInline ''"${key}"'')
+          (mkLuaInline ''hl.dsp.global("${shortcut}")'')
+          options
+        ];
+      };
+    in {
       wayland.windowManager.hyprland.settings = {
         on = lib.mkBefore [
           {
@@ -20,41 +30,44 @@
         # Caelestia's upstream dotfiles use Super on its own for the launcher.
         # The interrupt bindings keep Super+key and Super+mouse shortcuts from
         # opening the launcher after their normal action is handled.
-        bindi = lib.mkAfter [
-          {
-            _args = ["SUPER" "Super_L" "global" "caelestia:launcher"];
-          }
+        bind = lib.mkAfter [
+          (globalBind "SUPER + Super_L" "caelestia:launcher" {ignore_mods = true;})
+          (globalBind "SUPER + mouse:272" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse:273" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse:274" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse:275" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse:276" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse:277" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse_up" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
+          (globalBind "SUPER + mouse_down" "caelestia:launcherInterrupt" {
+            ignore_mods = true;
+            non_consuming = true;
+          })
         ];
 
-        bindin = lib.mkAfter [
-          {
-            _args = ["SUPER" "catchall" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:272" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:273" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:274" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:275" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:276" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse:277" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse_up" "global" "caelestia:launcherInterrupt"];
-          }
-          {
-            _args = ["SUPER" "mouse_down" "global" "caelestia:launcherInterrupt"];
-          }
-        ];
+        # `catchall` is a Hyprland keyword, not an XKB keysym. Keep it out of
+        # the Lua binding helper, which parses key combinations as keysyms.
       };
     };
   };
