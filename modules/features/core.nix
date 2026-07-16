0f1b8d2bc236b3740247c0ceafd621cd8e99b8f4
retr0astic <sreejiraj2399@gmail.com>
2026-07-16 22:35:58 +0400

    updated inseceure packages

--- modules/features/core.nix
+++ modules/features/core.nix
@@ -6,7 +6,7 @@
     nix.settings.experimental-features = ["nix-command" "flakes"];
     nix.settings.trusted-users = ["root"];
     nixpkgs.config.allowUnfree = true;
-    nixpkgs.config.permittedInsecurePackages = ["pnpm-10.29.2" "electron-39.8.10"];
+    nixpkgs.config.permittedInsecurePackages = ["pnpm-10.29.2" "electron-39.8.10" "electron-40.10.5"];
     environment.sessionVariables = {
       NIXOS_OZONE_WL = "1";
       SDL_VIDEODRIVER = "wayland";
@@ -14,6 +14,9 @@
       SAL_USE_VCLPLUGIN = "qt6";
     };
     programs.dconf.enable = true;
-    programs.nix-ld = {enable = true; libraries = [];};
+    programs.nix-ld = {
+      enable = true;
+      libraries = [];
+    };
   };
 }
