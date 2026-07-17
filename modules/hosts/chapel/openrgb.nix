47d1bb84a8d4a32822e565729950eb6237b63108
retr0astic <sreejiraj2399@gmail.com>
2026-07-17 15:21:47 +0400

    removed imv, installed loupe and swayimg

--- modules/hosts/chapel/openrgb.nix
+++ modules/hosts/chapel/openrgb.nix
@@ -10,8 +10,8 @@
     };
     home = {pkgs, ...}: {
       systemd.user.services.openrgb = {
-        Unit = {Description = "OpenRGB tray"; After = ["xdg-desktop-portal.service"];};
-        Service = {ExecStartPre = "${pkgs.coreutils}/bin/sleep 5"; ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized"; Restart = "on-failure"; RestartSec = 5;};
+        Unit = {Description = "OpenRGB tray"; After = ["graphical-session.target" "xdg-desktop-portal.service"];};
+        Service = {ExecStartPre = "${pkgs.coreutils}/bin/sleep 15"; ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized"; Restart = "on-failure"; RestartSec = 5;};
         Install = {WantedBy = ["graphical-session.target"];};
       };
     };
