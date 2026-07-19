0aa5117b52fc272bd4807f8e7c39e23ad1a915ce
retr0astic <sreejiraj2399@gmail.com>
2026-07-19 12:32:26 +0400

    added easyeffects

--- modules/features/packages.nix
+++ modules/features/packages.nix
@@ -1,5 +1,5 @@
 {...}: {
   config.retr0astic.features.packages.home = {pkgs, ...}: {
-    home.packages = with pkgs; [fastfetch fd htop ripgrep eza jq libsecret gh chromium obsidian];
+    home.packages = with pkgs; [fastfetch fd htop ripgrep eza jq libsecret gh chromium obsidian easyeffects];
   };
 }
