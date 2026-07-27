919ba8490a3d9bd514a0c1f45157dd4a31cf1d75
retr0astic <sreejiraj2399@gmail.com>
2026-07-27 11:19:43 +0400

    system update

--- modules/features/system-packages.nix
+++ modules/features/system-packages.nix
@@ -1,5 +1,5 @@
 {...}: {
   config.retr0astic.features.system-packages.system = {pkgs, ...}: {
-    environment.systemPackages = with pkgs; [vim bat wget sbctl git glib lm_sensors gsettings-desktop-schemas opencode dconf ddcutil seahorse gnome-keyring libsecret nwg-look adw-gtk3 qt6Packages.qt6ct mpv loupe swayimg kdePackages.dolphin kdePackages.ark kdePackages.kio-extras btop p7zip bubblewrap unrar];
+    environment.systemPackages = with pkgs; [vim bat wget sbctl git glib lm_sensors gsettings-desktop-schemas opencode dconf ddcutil seahorse gnome-keyring libsecret nwg-look adw-gtk3 qt6Packages.qt6ct mpv loupe swayimg kdePackages.dolphin kdePackages.ark kdePackages.kio-extras btop p7zip bubblewrap unrar smartmontools];
   };
 }
